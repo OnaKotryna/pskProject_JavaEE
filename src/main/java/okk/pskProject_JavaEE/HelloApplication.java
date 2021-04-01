ffcbde876ea321965054916427c677322912ec0f
Ona Kotryna <ok.kuncyte@gmail.com>
2021-04-01 14:26:11 +0300

    Initial commit

---
+++ src/main/java/okk/pskProject_JavaEE/HelloApplication.java
@@ -0,0 +1,9 @@
+package okk.pskProject_JavaEE;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
